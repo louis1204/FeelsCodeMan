2af847fb5c875214fa64f2cbd98e3b1cc7dcefab
Louis <Louis1204@gmail.com>
2015-08-05 09:08:46 -0700

    init

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ config = {
 
   // Production (Heroku)
   production: {
-    url: process.env.HEROKU_URL + '/blog',
+    url: 'http://louisuuu.herokuapp.com/blog',
     mail: {
       transport: 'SMTP',
       options: {

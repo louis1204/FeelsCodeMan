d6eed166926c51e37f6329205f405c4a6a44da38
Louis <Louis1204@gmail.com>
2015-08-07 09:22:38 -0700

    init

--- server.js
+++ server.js
@@ -8,17 +8,6 @@ fileStatusSocket = sockjs.createServer({}),
 chokidar = require('chokidar'),
 fs = require('fs');
 
-ghost({
-  config: path.join(__dirname, 'config.js')
-}).then(function (ghostServer) {
-  ghostServer.start();
-  ghostServer.get('/site', function (req, res) {
-    var buffer = fs.readFileSync('client/index.html');
-    var html = buffer.toString('utf-8');
-    res.send(html);
-  });
-});
-
 var watcher = chokidar.watch('gallery', {ignored: /[\/\\]\./, persistent: true});
 
 fileStatusSocket.on('connection', function(conn) {
@@ -49,6 +38,12 @@ fileStatusSocket.on('connection', function(conn) {
 
 fileStatusSocket.installHandlers(server, {prefix:'/filestatus'});
 
+app.get('/site', function (req, res) {
+  var buffer = fs.readFileSync('client/index.html');
+  var html = buffer.toString('utf-8');
+  res.send(html);
+});
+
 app.get('/thumbs/gallery/*', function(req, res, next) {
 	var src_path = __dirname + '/gallery/' + req.params[0];
     var dst_path = __dirname + '/thumbs/' + req.params[0];
@@ -71,3 +66,17 @@ app.get('/thumbs/gallery/*', function(req, res, next) {
 });
 
 app.use("/gallery",  express.static(__dirname + '/gallery'));
+ghost({
+  config: path.join(__dirname, 'config.js')
+}).then(function (ghostServer) {
+  ghostServer.start(app);
+});
+//
+//
+//
+// server.listen(process.env.PORT || 8000);
+// if (process.env.PORT) {
+//   console.log('Listening on localhost:' + process.env.PORT);
+// } else {
+//   console.log('Listening on localhost:8000');
+// }
